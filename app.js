42f4391905e6505499172bf0281e06b80bd5fe4a
alisher <alisher@gmail.com>
2024-09-17 21:15:22 +0500

    pense_3oy

---
+++ app.js
@@ -0,0 +1,8 @@
+// Hamburger va menyu elementlarini olamiz
+// const hamburger = document.querySelector(".header__hamburger");
+// const nav = document.querySelector(".header__nav");
+
+// // Hamburgerga bosilganda menyuni ochish/yopish funksiyasi
+// hamburger.addEventListener("click", () => {
+//   nav.classList.toggle("active"); // 'active' klassini qo'shadi yoki oladi
+// });
